32d011341b3c70fd601084ccd2eab5666ebfdf21
Makadam-ia <castanier.lucas34@gmail.com>
2026-08-29 10:49:07 +0200

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -3,7 +3,8 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Méthode non autorisée' });
   }
 
-  const n8nWebhookUrl = 'https://INSERER_URL_WEBHOOK_N8N_PRODUCTION';
+  const n8nWebhookUrl = 'https://https://portfoliohighgency.app.n8n.cloud/webhook/4a1614a7-25d5-461b-afbf-4e504720d70d
+';
 
   try {
     const response = await fetch(n8nWebhookUrl, {
